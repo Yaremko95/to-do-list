7abd7d9d67e296422f3d994c76d46c8b3d394e08
Tetiana Yaremko <tetianayaremko@gmail.com>
2022-10-18 17:58:14 +0700

    adds final logic

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-
-console.log("calling js")
+console.log("calling js");
 
 const getValue = () => {
-  let inputNode = document.getElementById("input-value")
-  console.log(inputNode.value)
-}
-
-const generateLi = function () {
-  const ul = document.querySelector("#list");
-  const li = document.createElement("li");
-  li.innerText = "some task";
-  ul.appendChild(li);
+  let inputNode = document.getElementById("input-value");
+  console.log(inputNode.value);
+  generateLi(inputNode.value);
 };
 
-generateLi();
-
+const generateLi = function (text) {
+  const ul = document.querySelector("#list");
+  const li = document.createElement("li");
+  li.innerText = text;
+  ul.appendChild(li);
+};
