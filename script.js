1f877597a2fca7df5a2a474b9f9c097e8af117c9
Tetiana Yaremko <tetianayaremko@gmail.com>
2022-10-18 17:39:32 +0700

    adds li generate function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("calling js");
+const generateLi = function () {
+  const ul = document.querySelector("#list");
+  const li = document.createElement("li");
+  li.innerText = "some task";
+  ul.appendChild(li);
+};
+
+generateLi();
